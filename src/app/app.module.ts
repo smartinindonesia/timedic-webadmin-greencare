80dc63abe39f623a7e1bde58fe7fa212c4494a74
Cakra Adipura W <adipurapunya@gmail.com>
2018-02-22 21:52:16 +0700

    First Commint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { LoginComponent } from './componets/login/login.component';
+import { RegisterComponent } from './componets/register/register.component';
+import { HomeComponent } from './componets/home/home.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
+import { ProfileComponent } from './componets/profile/profile.component';
+
+import {ValidateService} from './services/validate.service';
+import {AuthService} from './services/auth.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+import {AuthGuard} from './guards/auth.guard';
+import { FooterComponent } from './componets/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
