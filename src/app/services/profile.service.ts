049e8bc4d4ce92f6fdc1fe020835a925a4c1c16e
Cakra Adipura W <adipurapunya@gmail.com>
2018-03-02 19:28:25 +0700

    Fix profile and separate it's services

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
+@Injectable()
+export class ProfileService {
+
+  authToken: any;
+  user: any;
+
+  constructor(private http: Http) { }
+
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    this.loadUser();
+    headers.append('Authorization', this.authToken);
+    return this.http.get('https://timedic.id:8443/api/user/' + this.user.id, {headers: headers}).map(res => res.json());
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loadUser(){
+    const user = localStorage.getItem('user');
+    this.user = JSON.parse(user);
+  }
+
+}
