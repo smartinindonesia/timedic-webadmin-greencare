5b47eb3a887ba9d8e66de41a3793cf1752ff4899
yusfia <yusfia.hafidz@gmail.com>
2018-03-02 21:14:13 +0700

    + service and component

--- src/app/componets/caregiverlist/caregiverlist.component.ts
+++ src/app/componets/caregiverlist/caregiverlist.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CaregiverlistService} from '../../services/caregiverlist.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-caregiverlist',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaregiverlistComponent implements OnInit {
 
-  constructor() { }
+  careGiverList: Object;
+  page: number;
+  size: number;
+
+  constructor(private caregiverList: CaregiverlistService,
+              private router: Router,
+              private flashMessage: FlashMessagesService) {
+  }
 
   ngOnInit() {
+    this.getCareGiverList();
+  }
+
+  onClickNext(){
+    this.page++;
+  }
+
+  onClickPrevious(){
+    this.page--;
+  }
+
+  getCareGiverList() {
+    let param = {
+      'page': this.page,
+      'size': this.size,
+      'sort': 'ASC',
+      'sortField': 'id'
+    };
+    this.caregiverList.getCareGivers(param).subscribe(data => {
+      this.careGiverList = data;
+      console.log(data);
+    }, error => {
+      console.log(error);
+      return false;
+    });
+  }
+
+  addCareGiver(){
+
   }
 
 }
--- src/app/services/caregiverlist.service.ts
+++ src/app/services/caregiverlist.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
 
 @Injectable()
 export class CaregiverlistService {
 
-  constructor() { }
+  authToken: any;
+  user: any;
+
+  constructor(private http: Http) { }
+
+  getCareGivers(params) {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    let strpar = '?page='+params.page+'&size='+params.size+'&sort='+params.sort+'&sortField='+params.sortField;
+    return this.http.get('https://timedic.id:8443/api/caregiverswithpagination'+strpar, {headers: headers}).map(res => res.json());
+  }
+
+  registerCareGivers(params){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    return this.http.post('https://timedic.id:8443/api/caregiver', params, {headers: headers}).map(res => res.json());
+  }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
 
 }
